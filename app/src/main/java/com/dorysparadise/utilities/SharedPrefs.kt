2eae158cf668372a2c8c0b507cf6ed650a082cb4
Ferkhan <ferzzz@correo.com>
2024-05-24 23:01:33 -0500

    editar shared preferences

--- app/src/main/java/com/dorysparadise/utilities/SharedPrefs.kt
+++ app/src/main/java/com/dorysparadise/utilities/SharedPrefs.kt
@@ -1,16 +1,42 @@
 package com.dorysparadise.utilities
 
 import android.content.Context
+import com.dorysparadise.bl.models.Usuario
 
 // Guardar datos en la memoria del dispositivo (Shared Preferences)
 class SharedPrefs(val contexto: Context) {
     val SHARED_NAME = "BD_local"
-    val SHARED_ID_USUARIO = "id_usuario"
+    val SHARED_USUARIO_ID = "usuario_id"
+    val SHARED_USUARIO_NOMBRE = "usuario_nombre"
+    val SHARED_USUARIO_ROL = "usuario_rol"
+    val SHARED_USUARIO_CORREO = "usuario_correo"
+    val SHARED_USUARIO_CLAVE = "usuario_clave"
+    val SHARED_USUARIO_IMG = "usuario_img"
+    val SHARED_USUARIO = "usuario"
     val almacen = contexto.getSharedPreferences(SHARED_NAME, 0)
 
     fun setIdUsuario(id: Int) {
-        almacen.edit().putInt(SHARED_ID_USUARIO, id).apply()
+        almacen.edit().putInt(SHARED_USUARIO_ID, id).apply()
+    }
+    fun getIdUsuario(): Int = almacen.getInt(SHARED_USUARIO_ID, 0)
+
+    fun setUsuario(usuario: Usuario) {
+        almacen.edit().putInt(SHARED_USUARIO_ID, usuario.id).apply()
+        almacen.edit().putString(SHARED_USUARIO_CORREO, usuario.correo).apply()
+        almacen.edit().putString(SHARED_USUARIO_ROL, usuario.rol).apply()
+        almacen.edit().putString(SHARED_USUARIO_NOMBRE, usuario.nombre).apply()
+        almacen.edit().putString(SHARED_USUARIO_CLAVE, usuario.clave).apply()
+        almacen.edit().putString(SHARED_USUARIO_IMG, usuario.imgRuta).apply()
     }
 
-    fun getIdUsuario(): Int = almacen.getInt(SHARED_ID_USUARIO, 0)
+    fun getUsuario() : Usuario {
+        return Usuario(
+            almacen.getInt(SHARED_USUARIO_ID, 0),
+            almacen.getString(SHARED_USUARIO_ROL, "")!!,
+            almacen.getString(SHARED_USUARIO_NOMBRE, "")!!,
+            almacen.getString(SHARED_USUARIO_CORREO, "")!!,
+            almacen.getString(SHARED_USUARIO_CLAVE, "")!!,
+            almacen.getString(SHARED_USUARIO_IMG, "")!!
+        )
+    }
 }
